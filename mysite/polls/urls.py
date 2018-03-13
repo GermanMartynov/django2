cd18bc5109efbc1c786073819f505e79bcd5fd04
german <gm@parquet.ru>
2018-03-13 14:47:48 +0300

    конец главы 3

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls' # пространство имен URL
 urlpatterns = [
     # ex: /polls/
+    # вызов из шаблона: {% url 'polls:index %}
     path('', views.index, name='index'),
     # ex: /polls/5/
+    # вызов из шаблона: {% url 'polls:detail' question.id %}
     path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
